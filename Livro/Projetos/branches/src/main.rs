27653a06ea42fa1efb542983eea34792d27ee249
LaLunaInSky <moonaflordapele@gmail.com>
2025-05-10 09:50:08 -0300

    10/05/2025 09:50

--- Livro/Projetos/branches/src/main.rs
+++ Livro/Projetos/branches/src/main.rs
@@ -10,22 +10,22 @@ fn main() {
     // if number != 0 {
     //     println!("number was something other than zero");
     // }
-
-    // if number % 4 == 0 {
-    //     println!("number is divisible by 4");
-    // } else if number % 3 == 0 {
-    //     println!("number is divisible by 3");
-    // } else if number % 2 == 0 {
-    //     println!("number is divisible by 2");
-    // } else {
-    //     println!("number is not divisible by 4, 3, or 2");
-    // }
-
+    
     let condition = true;
     
-    // let number = if condition { 5 } else { 6 };
-
-    let number = if condition { 5 } else { "six" };
-
+    let number = if condition { 5 } else { 6 };
+    
+    // let number = if condition { 5 } else { "six" };
+    
     println!("The value if number is: {number}");
+    
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 }
