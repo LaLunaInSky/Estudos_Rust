e9a3e04679284bfbf9385c076ac27629c561ce35
LaLunaInSky <moonaflordapele@gmail.com>
2025-05-12 08:46:04 -0300

    12/05/2025 08:46

--- Livro/Projetos/loops/src/main.rs
+++ Livro/Projetos/loops/src/main.rs
@@ -11,6 +11,9 @@ fn main() {
 
     println!("The result is {result}");
 
+    ////
+    println!();
+
     let mut count = 0;
 
     'counting_up: loop {
@@ -30,5 +33,46 @@ fn main() {
 
         count += 1;
     }
+
     println!("End count = {count}");
+
+    /////
+    println!();
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    //////
+    println!();
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("The value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    println!();
+
+    for element in a {
+        println!("The value is: {element}");
+    }
+
+    /////
+    println!();
+
+    for número in (1..4).rev() {
+        println!("{número}!");
+    }
+
+    println!("LIFTOFF!!!")
 }
