5839f90db09fe6322f2e26c3b23e6d3e96eec00d
LaLunaInSky <moonaflordapele@gmail.com>
2025-05-18 14:04:54 -0300

    18/05/2025 14:04

--- Livro/Projetos/rectangles/src/main.rs
+++ Livro/Projetos/rectangles/src/main.rs
@@ -16,6 +16,21 @@ impl Rectangle {
     }
 }
 
+impl Rectangle {
+    fn can_hold(&self, other_rect: &Rectangle) -> bool {
+        self.width > other_rect.width && self.height > other_rect.height
+    }
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     // let width_1 = 30;
     // let height_1 = 50;
@@ -40,8 +55,40 @@ fn main() {
 
     if rect_1.width() {
         println!(
-            "The rectangle has a nonzero width; it is {}",
+            "\nThe rectangle has a nonzero width; it is {}",
             rect_1.width
         );
     }
+
+    //
+
+    let rect_01 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect_02 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect_03 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!(
+        "\nCan rect_01 hold rect_02? {}", rect_01.can_hold(&rect_02)
+    );
+
+    println!(
+        "Can rect_01 hold rect_03? {}", rect_01.can_hold(&rect_03)
+    );
+
+    //
+    let sq = Rectangle::square(3);
+
+    println!(
+        "\nsq is {sq:?}"
+    );
 }
