19989c2c495eb914010eeef980e0be700cdfb8e6
LaLunaInSky <moonaflordapele@gmail.com>
2025-05-19 22:28:47 -0300

    19/05/2025 22:28

--- Livro/Anotações/Capítulo_6/Cap_6.3/main.rs
+++ Livro/Anotações/Capítulo_6/Cap_6.3/main.rs
@@ -1,3 +1,77 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
+    let config_max = Some(3u8);
+
+    // match config_max {
+    //     Some(max) => println!(
+    //         "The maximum is configured to be {max}"
+    //     ),
+    //     _ => (),
+    // }
+
+    if let Some(max) = config_max {
+        println!(
+            "The maximum is configured to be {max}"
+        )
+    };
+
+    //
+    println!();
+
+    let mut count = 0;
     
+    let mut coin = Coin::Penny;
+    
+    value_in_cents(
+        coin, count
+    );
+
+    coin = Coin::Nickel;
+
+    value_in_cents(
+        coin, count
+    );
+
+    coin = Coin::Dime;
+
+    value_in_cents(
+        coin, count
+    );
+
+    coin = Coin::Quarter(UsState::Alabama);
+
+    value_in_cents(
+        coin, count
+    );
+
+    coin = Coin::Quarter(UsState::Alaska);
+
+    value_in_cents(
+        coin, count
+    );
+
+    println!(
+        "The count is {}", count
+    );
+}
+
+fn value_in_cents(coin: Coin, count: &mut u32) {
+    match coin {
+        Coin::Quarter(state) => println!(
+            "State quarter from {state:?}!"
+        ),
+        _ => *count += 1,
+    }
 }
