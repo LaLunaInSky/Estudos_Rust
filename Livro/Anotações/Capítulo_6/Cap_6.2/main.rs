3a31fb4db28cbf1912c57e498552643748642f50
LaLunaInSky <moonaflordapele@gmail.com>
2025-05-19 21:38:47 -0300

    19/05/2025 21:38

--- Livro/Anotações/Capítulo_6/Cap_6.2/main.rs
+++ Livro/Anotações/Capítulo_6/Cap_6.2/main.rs
@@ -54,6 +54,16 @@ fn main() {
     println!(
         "Five variable is: {five:?}\nSix variable is: {six:?}\nNone variable is: {none:?}"
     );
+
+    //
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        //oth,er => move_player(other)
+        //_ => reroll(),
+        _ => (),
+    }
 }
 
 fn plus_one(x: Option<i32>) -> Option<i32> {
@@ -62,3 +72,8 @@ fn plus_one(x: Option<i32>) -> Option<i32> {
         Some(i) => Some(i + 1),
     }
 }
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+//fn move_player(num_spaces: u8) {}
+//fn reroll() {}
