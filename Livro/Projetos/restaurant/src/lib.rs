c75595b39c4bba76025d5b6b6c0b77cdbe98ddaf
LaLunaInSky <moonaflordapele@gmail.com>
2025-05-21 20:22:08 -0300

    21/05/2025 20:22

--- Livro/Projetos/restaurant/src/lib.rs
+++ Livro/Projetos/restaurant/src/lib.rs
@@ -1,15 +1,23 @@
-mod front_od_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
+        // fn seat_at_table() {}
     }
 
-    mod serving {
-        fn take_order() {}
+    // mod serving {
+    //     fn take_order() {}
 
-        fn serve_order() {}
+    //     fn serve_order() {}
 
-        fn take_payment() {}
-    }
+    //     fn take_payment() {}
+    // }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house:: hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
