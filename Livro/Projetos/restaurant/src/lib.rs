6ff0b060431517b4647c78dc2055dba443169e1b
LaLunaInSky <moonaflordapele@gmail.com>
2025-05-22 14:45:23 -0300

    21/05/2025 14:45

--- Livro/Projetos/restaurant/src/lib.rs
+++ Livro/Projetos/restaurant/src/lib.rs
@@ -1,52 +1,13 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        // fn seat_at_table() {}
-    }
-
-    // mod serving {
-    //     fn take_order() {}
-
-    //     fn serve_order() {}
-
-    //     fn take_payment() {}
-    // }
-}
-
-mod back_of_house {
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
-}
+mod front_of_house;
+mod back_of_house;
 
 pub use crate::front_of_house::hosting;
+
 // pub use crate::back_of_house::Appetizer;
 // pub use crate::back_of_house::Breakfast;
 
 pub use crate::back_of_house::{Appetizer, Breakfast};
+
 
 pub fn eat_at_restaurant() {
     // Absolute path
