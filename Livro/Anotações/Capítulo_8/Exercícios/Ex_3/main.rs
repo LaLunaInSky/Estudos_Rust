145de3a1f9242a2caf825778b9118bf9c896d7b1
LaLunaInSky <moonaflordapele@gmail.com>
2025-05-25 14:03:00 -0300

    25/05/2025 14:02

--- Livro/Anotações/Capítulo_8/Exercícios/Ex_3/main.rs
+++ Livro/Anotações/Capítulo_8/Exercícios/Ex_3/main.rs
@@ -1,39 +1,21 @@
-use std::{collections::HashMap, io};
-
-struct OpçõesDoMenu {
-    
-}
-
-fn limpar_o_terminal() {
-    print!("\x1B[2J\x1B[1;1H");
-}
-
-fn menu_principal_de_opções_da_empresa() -> i8 {
-    loop {
-        limpar_o_terminal();
-
-        println!(
-            "- Bem vindo a Empresa XoXo -\n\n[ 1 ] Ver o quadro de funcinários de toda a empresa.\n[ 2 ] Ver o quadro de funcionários de um determinado setor.\n[ 3 ] Adicionar um funcionário a um setor.\n[ 4 ] Criar um novo setor na empresa.\n\nO que você gostaria?" 
-        );
-
-        let mut opção_escolhida_do_menu_principal = String::new();
-
-        io::stdin().read_line(&mut opção_escolhida_do_menu_principal).expect("Falha ao ler o input");
-
-        let opção_escolhida_do_menu_principal: i8 = opção_escolhida_do_menu_principal.trim().parse().expect("O input não é um número!");
-
-        if opção_escolhida_do_menu_principal > 0 && opção_escolhida_do_menu_principal <= 4 {
-            return opção_escolhida_do_menu_principal;
-        }
-    }
-}
-
-fn
+use std::collections::HashMap;
 
 fn main() {
-    let opção_escolhida_do_menu_principal = menu_principal_de_opções_da_empresa();
+    let mut mapa_de_funcionários_da_empresa = HashMap::new();
 
-    let mut mapa_dados_de_funcionários_da_empresa_por_setor = HashMap::new();
+    let setores_da_empresa = vec![
+        "administração", "financeiro", "suporte técnico", "rh", "atendimento ao público", "venda"
+    ];
 
-    println!("{mapa_dados_de_funcionários_da_empresa_por_setor:?}");
+    for setor in &setores_da_empresa {
+        mapa_de_funcionários_da_empresa.entry(setor).or_insert(0);
+    };
+
+    println!(
+        "{mapa_de_funcionários_da_empresa:?}"
+    );
+
+    println!(
+        "{setores_da_empresa:?}"
+    );
 }
