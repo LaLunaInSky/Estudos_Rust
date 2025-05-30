1d559e7c23db5b1af618b0648ece68da87d103f8
LaLunaInSky <moonaflordapele@gmail.com>
2025-05-30 17:53:24 -0300

    30/05/2025 17:53

--- Livro_Rust/Projetos/aggregator/src/lib.rs
+++ Livro_Rust/Projetos/aggregator/src/lib.rs
@@ -6,11 +6,25 @@ pub trait Summary {
     }
 }
 
-pub fn notify(item: &impl Summary) {
+// pub fn notify(item: &impl Summary) {
+//     println!(
+//         "Breaking news! {}", item.summarize()
+//     );
+// }
+
+pub fn notify<T: Summary>(item: &T) {
     println!(
         "Breaking news! {}", item.summarize()
     );
 }
+
+// pub fn notify(item1: &impl Summary, item2: &impl Summary) {
+
+// pub fn notify<T: Summary>(item1: &T, item2: &T) {
+
+// pub fn notify(item: &(impl Summary + Display)) {
+
+// pub fn notify<T: Summary + Display>(item: &T) {
 
 pub struct NewsArticle {
     pub headline: String,
