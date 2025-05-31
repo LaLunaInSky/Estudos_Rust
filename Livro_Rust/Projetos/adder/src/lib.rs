2cb65ca54f7eaa161090f7ff96556491ddca71d0
LaLunaInSky <moonaflordapele@gmail.com>
2025-05-31 19:14:33 -0300

    31/05/2025 19:14

--- Livro_Rust/Projetos/adder/src/lib.rs
+++ Livro_Rust/Projetos/adder/src/lib.rs
@@ -65,11 +65,12 @@ pub fn prints_and_returns_10(a: i32) -> i32 {
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn exploration() {
-    //     let result = add(2, 2);
-    //     assert_eq!(result, 4);
-    // }
+    #[test]
+    #[ignore]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
 
     #[test]
     fn it_works() -> Result<(), String> {
@@ -84,10 +85,11 @@ mod tests {
         }
     }
 
-    // #[test]
-    // fn another() {
-    //     panic!("Make this test fail");
-    // }
+    #[test]
+    #[ignore]
+    fn another() {
+        panic!("Make this test fail");
+    }
 
     #[test]
     fn larger_can_hold_smaller() {
@@ -147,8 +149,27 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn this_test_will_fail() {
         let value = prints_and_returns_10(8);
         assert_eq!(value, 5);
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        let result = add_two(3);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn one_hundred() {
+        let result = add_two(100);
+        assert_eq!(result, 102);
     }
 }
