494dc6e4ddcc426e33f89da3cd6ca23d2ffc538d
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-01 21:06:41 -0300

    01/06/2025 21:06

--- Livro_Rust/Projetos/minigrep/src/main.rs
+++ Livro_Rust/Projetos/minigrep/src/main.rs
@@ -1,15 +1,29 @@
 use std::{env, fs};
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = &args[1].clone();
+        let file_path = &args[2].clone();
+
+        Config { query: query.to_string(), file_path: file_path.to_string() }
+    }
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {0}", config.query);
+    println!("In file {0}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("\nWith text:\n{contents}");
