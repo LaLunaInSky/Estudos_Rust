de79c1845ac727fb6985fcd104204ff9cd50e63b
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-05 19:08:42 -0300

    05/06/2025 19:08

---
+++ Livro_Rust/Projetos/my_cargo/src/lib.rs
@@ -0,0 +1,38 @@
+/// Adds one to the number given.
+/// 
+/// # Examples
+/// 
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+/// 
+/// assert_eq!(6, answer);
+/// ```
+///
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_works_add_one() {
+        let arg = 5;
+
+        let answer = add_one(arg);
+
+        assert_eq!(answer, 6);
+    }
+}
