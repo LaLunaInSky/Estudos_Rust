fd5ad7ceee1ad2c7d4cfbb27cbe8727c5db897e9
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-08 13:43:55 -0300

    08/06/2025 13:43

---
+++ Livro_Rust/Projetos/deref_cap/src/main.rs
@@ -0,0 +1,36 @@
+use std::{
+    process::Command,
+    ops::Deref,
+};
+
+fn clean_terminal() {
+    Command::new("clear").status().unwrap();
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }    
+}
+
+fn main() {
+    clean_terminal();
+    
+    let x = 5;
+    
+    // let y = &x;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
