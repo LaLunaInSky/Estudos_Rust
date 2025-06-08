ed42d72301cdf92dd06f77120d79f7fa2f12f919
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-07 22:20:13 -0300

    07/06/2025 22:20

--- Gtk4/Projetos/memory-management/src/main.rs
+++ Gtk4/Projetos/memory-management/src/main.rs
@@ -2,15 +2,29 @@ use gtk::{
     prelude::*,
     glib,
     Application,
-    ApplicationWindow
+    ApplicationWindow,
+    Button
 };
 
 const APP_ID: &str = "org.gtk_rs.MemoryManagement";
 
 fn build_ui(app: &Application) {
+    let button_increase = Button::builder()
+        .label("Increase")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let mut number = 0;
+
+    button_increase.connect_clicked(|_| number += 1);
+
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Memory Management")
+        .child(&button_increase)
         .build();
 
     window.present();
