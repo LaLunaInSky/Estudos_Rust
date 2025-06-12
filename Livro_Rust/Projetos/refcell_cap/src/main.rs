6584aa6bd81f761e0ab5d3a6cdbfdb9d363aa6fa
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-11 21:24:59 -0300

    11/06/2025 21:24

--- Livro_Rust/Projetos/refcell_cap/src/main.rs
+++ Livro_Rust/Projetos/refcell_cap/src/main.rs
@@ -1,4 +1,18 @@
-use std::process::Command;
+#[derive(Debug)]
+enum List {
+    Cons (Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use std::{
+    process::Command,
+    cell::RefCell,
+    rc::Rc,
+};
+use crate::List::{
+    Cons,
+    Nil,
+};
 
 fn clean_terminal() {
     Command::new("clear").status().unwrap();
@@ -9,4 +23,34 @@ fn main() {
 
     let mut x = 5;
     let _y = &mut x;
+
+    //
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(
+        Cons(
+            Rc::clone(&value),
+            Rc::new(Nil)
+        )
+    );
+
+    let b = Cons(
+        Rc::new(
+            RefCell::new(3)
+        ),
+        Rc::clone(&a)
+    );
+
+    let c = Cons(
+        Rc::new(
+            RefCell::new(4)
+        ),
+        Rc::clone(&a)
+    );
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
 }
