2a4da148f8d5f33a92a0e85c3972360c8503f743
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-11 22:09:14 -0300

    11/06/2025 22:09

---
+++ Livro_Rust/Projetos/cycle_cap/src/main.rs
@@ -0,0 +1,85 @@
+use std::{
+    process::Command,
+    cell::RefCell,
+    rc::Rc
+};
+
+use crate::List::{
+    Cons,
+    Nil
+};
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
+fn clean_terminal() {
+    Command::new("clear").status().unwrap();
+}
+
+fn main() {
+    clean_terminal();
+
+    let a = Rc::new(
+        Cons(
+            5, RefCell::new(
+                Rc::new(Nil)
+            )
+        )
+    );
+
+    println!(
+        "a initial rc count = {}", Rc::strong_count(&a)
+    );
+
+    println!(
+        "a next item = {:?}", a.tail()
+    );
+
+    let b = Rc::new(
+        Cons(
+            10, RefCell::new(
+                Rc::clone(&a)
+            )
+        )
+    );
+
+    println!(
+        "\na rc count after b creation = {}", Rc::strong_count(&a)
+    );
+
+    println!(
+        "b initial rc count = {}", Rc::strong_count(&b)
+    );
+
+    println!(
+        "b next item = {:?}", b.tail()
+    );
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!(
+        "\nb rc count after changing a = {}", Rc::strong_count(&b)
+    );
+
+    println!(
+        "a rc count after changing a = {}", Rc::strong_count(&a)
+    );
+
+    // println!(
+    //     "a next item = {:?}", a.tail()
+    // );
+}
