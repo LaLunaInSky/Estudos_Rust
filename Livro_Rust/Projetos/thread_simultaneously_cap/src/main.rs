dc4e46033203168ae506ece56fa1c42b2cdef880
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-13 19:56:00 -0300

    13/06/2025 19:55

--- Livro_Rust/Projetos/thread_simultaneously_cap/src/main.rs
+++ Livro_Rust/Projetos/thread_simultaneously_cap/src/main.rs
@@ -11,15 +11,30 @@ fn clean_terminal() {
 fn main() {
     clean_terminal();
 
-    thread::spawn(|| {
+    let handle_01 = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {i} from the spawned thread!");
             thread::sleep(Duration::from_millis(1));
         }
     });
 
+    // handle_01.join().unwrap();
+
     for i in 1..5 {
         println!("hi number {i} from the main thread!");
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle_01.join().unwrap();
+
+    //
+    println!();
+
+    let v = vec![1, 2, 3];
+
+    let hanlde_02 = thread::spawn(|| {
+        println!("Here's a vector: {v:?}");
+    });
+
+    hanlde_02.join().unwrap();
 }
