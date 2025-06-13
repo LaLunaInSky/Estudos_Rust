1c30c61184f61b720e08f23f53be9d305f8f8699
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-13 19:47:13 -0300

    13/06/2025 19:47

---
+++ Livro_Rust/Projetos/thread_simultaneously_cap/src/main.rs
@@ -0,0 +1,25 @@
+use std::{
+    process::Command,
+    time::Duration,
+    thread
+};
+
+fn clean_terminal() {
+    Command::new("clear").status().unwrap();
+}
+
+fn main() {
+    clean_terminal();
+
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {i} from the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+}
