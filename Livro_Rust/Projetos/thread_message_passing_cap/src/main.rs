fb10f09f7966f9d0a3d96bc99fa698c1bf5e3a32
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-13 21:34:25 -0300

    13/06/2025 21:34

--- Livro_Rust/Projetos/thread_message_passing_cap/src/main.rs
+++ Livro_Rust/Projetos/thread_message_passing_cap/src/main.rs
@@ -13,6 +13,8 @@ fn main() {
     clean_terminal();
 
     let (tx, rx) = mpsc::channel();
+    
+    let tx1 = tx.clone();
 
     thread::spawn(move || {
         let vals = vec![
@@ -22,6 +24,20 @@ fn main() {
             String::from("thread"),
         ];
         
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
         for val in vals {
             tx.send(val).unwrap();
             thread::sleep(Duration::from_secs(1));
