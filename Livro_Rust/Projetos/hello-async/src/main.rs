1e3da6a666daef86cdc8324c3a2f56150153bf55
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-15 23:38:56 -0300

    15/06/2025 23:38

--- Livro_Rust/Projetos/hello-async/src/main.rs
+++ Livro_Rust/Projetos/hello-async/src/main.rs
@@ -1,4 +1,13 @@
 use std::process::Command;
+use trpl::Html;
+
+async fn page_title(url: &str) -> Option<String> {
+    let response = trpl::get(url).await;
+    let response_text = response.text().await;
+    Html::parse(&response_text)
+        .select_first("title")
+        .map(|title_element| title_element.inner_html())
+}
 
 fn clean_terminal() {
     Command::new("clear").status().unwrap();
