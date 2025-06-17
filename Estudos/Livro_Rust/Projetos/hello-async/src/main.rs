b51948d95665c0b8ceeaabac4641853e49a2eb85
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-17 19:40:07 -0300

    17/06/2025 19:40

--- Estudos/Livro_Rust/Projetos/hello-async/src/main.rs
+++ Estudos/Livro_Rust/Projetos/hello-async/src/main.rs
@@ -1,5 +1,8 @@
 use std::process::Command;
-use trpl::Html;
+use trpl::{
+    Html,
+    Either
+};
 
 enum PageTitleFuture<'a> {
     Initial { url: &'a str },
@@ -7,12 +10,14 @@ enum PageTitleFuture<'a> {
     TextAwaitPoint { response: trpl::Response },
 }
 
-async fn page_title(url: &str) -> Option<String> {
-    let response = trpl::get(url).await;
-    let response_text = response.text().await;
-    Html::parse(&response_text)
-        .select_first("title")
-        .map(|title_element| title_element.inner_html())
+async fn page_title(url: &str) -> (&str, Option<String>) {
+    let text = trpl::get(url).await.text().await;
+    
+    let title = Html::parse(&text)
+                    .select_first("title")
+                    .map(|title| title.inner_html());
+    
+    (url, title)
 }
 
 fn clean_terminal() {
@@ -25,11 +30,22 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     trpl::run(async {
-        let url = &args[1];
+        let title_fut_1 = page_title(&args[1]);
+        let title_fut_2 = page_title(&args[2]);
 
-        match page_title(url).await {
-            Some(title) => println!("The title for {url} was {title}"),
-            None => println!("{url} had no title"),
+        let (url, maybe_title) = match trpl::race(
+            title_fut_1,
+            title_fut_2
+        ).await {
+            Either::Left(left) => left,
+            Either::Right(right) => right,
+        };
+
+        println!("{url} returned first");
+
+        match maybe_title {
+            Some(title) => println!("It's page title is: '{title}'"),
+            None => println!("It's title could not be parsed."),
         }
     })
 }
