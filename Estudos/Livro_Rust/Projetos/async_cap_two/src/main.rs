b3cea873e5fe49a552b853699dbf1c5205a86fd0
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-17 20:22:20 -0300

    17/06/2025 20:22

---
+++ Estudos/Livro_Rust/Projetos/async_cap_two/src/main.rs
@@ -0,0 +1,30 @@
+use std::{
+    process::Command,
+    time::Duration
+};
+
+fn clean_terminal() {
+    Command::new("clear").status().unwrap();
+}
+
+fn main() {
+    clean_terminal();
+
+    trpl::run(async {
+        let fut_01 = async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let fut_02 = async {
+            for i in 1..5 {
+                println!("hi number {i} from the second task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        trpl::join(fut_01, fut_02).await;
+    });
+}
