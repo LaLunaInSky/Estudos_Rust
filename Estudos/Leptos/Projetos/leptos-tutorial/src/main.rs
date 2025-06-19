36b7c7e62ef72c4b635ab400604306420a4604e6
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-19 19:04:37 -0300

    19/06/2025 19:04

--- Estudos/Leptos/Projetos/leptos-tutorial/src/main.rs
+++ Estudos/Leptos/Projetos/leptos-tutorial/src/main.rs
@@ -1,6 +1,33 @@
 use leptos::{
-    mount::mount_to_body
+    mount::mount_to_body,
+    prelude::*
 };
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        <button
+            on:click=move |_| *set_count.write() += 1
+        >
+            "Click me: "
+            {count}
+        </button>
+        <p>
+            <strong>"Reactive: "</strong>
+            {move || count.get()}
+        </p>
+        <p>
+            <strong>"Reactive shorthand: "</strong>
+            {count}
+        </p>
+        <p>
+            <strong>"Not reactive: "</strong>
+            {count.get()}
+        </p>
+    }
+}
 
 fn main() {
     mount_to_body(App);
