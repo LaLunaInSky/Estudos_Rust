4980b5dd2ed11b5394d64ff0f5bdc6f68dce53e9
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-20 19:55:38 -0300

    20/06/2025 19:55

--- Estudos/Livro_Rust/Projetos/async_cap_two/src/main.rs
+++ Estudos/Livro_Rust/Projetos/async_cap_two/src/main.rs
@@ -10,21 +10,41 @@ fn clean_terminal() {
 fn main() {
     clean_terminal();
 
+    // trpl::run(async {
+    //     let fut_01 = async {
+    //         for i in 1..10 {
+    //             println!("hi number {i} from the first task!");
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     };
+
+    //     let fut_02 = async {
+    //         for i in 1..5 {
+    //             println!("hi number {i} from the second task!");
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     };
+
+    //     trpl::join(fut_01, fut_02).await;
+    // });
+
     trpl::run(async {
-        let fut_01 = async {
-            for i in 1..10 {
-                println!("hi number {i} from the first task!");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
+        let (tx, mut rx) = trpl::channel();
+
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("future"),
+        ];
+        
+        for val in vals {
+            tx.send(val).unwrap();
+            trpl::sleep(Duration::from_millis(500)).await;
         };
 
-        let fut_02 = async {
-            for i in 1..5 {
-                println!("hi number {i} from the second task!");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        };
-
-        trpl::join(fut_01, fut_02).await;
-    });
+        while let Some(value) = rx.recv().await {
+            println!("received '{value}'");
+        }
+    })
 }
