dd9a00631ba398307c7dec9fa68c70a66d33460c
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-20 20:41:26 -0300

    20/06/2025 20:41

--- Estudos/Livro_Rust/Projetos/async_cap_three/src/main.rs
+++ Estudos/Livro_Rust/Projetos/async_cap_three/src/main.rs
@@ -1,4 +1,11 @@
-use std::process::Command;
+use std::{
+    process::Command,
+    time::Duration,
+    pin::{
+        Pin,
+        pin
+    }
+};
 
 fn clean_terminal() {
     Command::new("clear").status().unwrap();
@@ -7,5 +14,60 @@ fn clean_terminal() {
 fn main() {
     clean_terminal();
     
-    println!("Hello, world!");
+    trpl::run(async {
+        let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone();
+        let tx1_fut = pin!(async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+            
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
+
+        let rx_fut = pin!(async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
+            }
+        });
+
+        let tx_fut = pin!(async move {
+            let vals = vec! [
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        });
+
+        // trpl::join3(tx1_fut, tx_fut, rx_fut).await;
+
+        let futures: Vec<Pin<&mut dyn Future<Output = ()>>> = vec![tx1_fut, tx_fut, rx_fut];
+
+        trpl::join_all(futures).await;
+    });
+
+    //
+    println!();
+
+    trpl::run(async {
+        let a = async { 1u32 };
+        let b = async { "Hello!" };
+        let c = async { true };
+
+        let (a_result, b_result, c_result) = trpl::join!(a, b, c);
+        println!("{a_result}, {b_result}, {c_result}");
+    })
 }
