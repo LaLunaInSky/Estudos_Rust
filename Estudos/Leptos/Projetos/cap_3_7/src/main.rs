e2f65ece965985de19296374cff877486a926ec8
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-23 04:15:07 -0300

    23/06/2025 04:15

---
+++ Estudos/Leptos/Projetos/cap_3_7/src/main.rs
@@ -0,0 +1,18 @@
+use leptos::{
+    mount::mount_to_body,
+    prelude::*
+};
+
+fn App() -> impl IntoView {
+    view! {
+        <h1>
+            "Cap_3_7 Leptos"
+        </h1>
+    }
+}
+
+fn main() {
+    console_error_panic_hook::set_once();
+
+    mount_to_body(App);
+}
