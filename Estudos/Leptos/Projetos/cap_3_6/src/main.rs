b3d4c15b91ff3794bf867f1ff1a0628864474ce8
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-23 03:29:56 -0300

    23/06/2025 03:29

--- Estudos/Leptos/Projetos/cap_3_6/src/main.rs
+++ Estudos/Leptos/Projetos/cap_3_6/src/main.rs
@@ -10,94 +10,99 @@ fn App() -> impl IntoView {
     let spam_me = RwSignal::new(true);
 
     view! {
-        <h1>
-            "Cap_3_6 Leptos"
-        </h1>
-        <div>
-            <label>
-                "Enter an your name: "
-                <input
-                    type="text"
-                    // on:input:target=move |ev| {
-                    //     set_name.set(ev.target().value());
-                    // }
-                    // prop:value=name
-
-                    bind:value=(name, set_name)
-                    style="display: block"
-                />
-            </label>
-            <label
-                style="display: block; margin-top: 1em"
-            >
-                "Enter an your email: "
-                <input
-                    type="email"
-                    style="display: block"
-                    bind:value=email
-                />
-            </label>
-            <label>
-                "Please send me lots of spam email."
-                <input 
-                    type="checkbox"
-                    bind:checked=spam_me
-                />
-            </label>
-            <fieldset
-                style="margin-top: 1em; width: 10em; display: flex; flex-direction: column; align-items: center"
-            >
-                <legend>
-                    "What's an your favorite color?"
-                </legend>
-                <label>
-                    "Red"
-                    <input 
-                        type="radio"
-                        name="color"
-                        value="red"
-                        bind:group=favorite_color
-                    />
-                </label>
-                <label>
-                    "Green"
-                    <input 
-                        type="radio"
-                        name="color"
-                        value="green"
-                        bind:group=favorite_color
-                    />
-                </label>
-                <label>
-                    "Blue"
-                    <input 
-                        type="radio"
-                        name="color"
-                        value="blue"
-                        bind:group=favorite_color
-                    />
-                </label>
-            </fieldset>
+        <div
+            style="display: flex; flex-direction: column; align-items: center"
+        >
+            <h1>
+                "Cap_3_6 Leptos"
+            </h1>
             <div>
-                <h2>
-                    "Results"
-                </h2>
-                <p>
-                    "Your favorite color is: " {favorite_color}
-                </p>
-                <p>
-                    "Name is: " {name}
-                </p>
-                <p>
-                    "Email is: " {email}
-                </p>
-                <Show
-                    when=move || spam_me.get()
+                <label>
+                    "Enter an your name: "
+                    <input
+                        type="text"
+                        
+                        // on:input:target=move |ev| {
+                        //     set_name.set(ev.target().value());
+                        // }
+                        // prop:value=name
+
+                        bind:value=(name, set_name)
+                        style="display: block"
+                    />
+                </label>
+                <label
+                    style="display: block; margin-top: 1em"
                 >
+                    "Enter an your email: "
+                    <input
+                        type="email"
+                        style="display: block"
+                        bind:value=email
+                    />
+                </label>
+                <label>
+                    "Please send me lots of spam email."
+                    <input
+                        type="checkbox"
+                        bind:checked=spam_me
+                    />
+                </label>
+                <fieldset
+                    style="margin-top: 1em; width: 10em; display: flex; flex-direction: column; align-items: center"
+                >
+                    <legend>
+                        "What's an your favorite color?"
+                    </legend>
+                    <label>
+                        "Red"
+                        <input
+                            type="radio"
+                            name="color"
+                            value="red"
+                            bind:group=favorite_color
+                        />
+                    </label>
+                    <label>
+                        "Green"
+                        <input
+                            type="radio"
+                            name="color"
+                            value="green"
+                            bind:group=favorite_color
+                        />
+                    </label>
+                    <label>
+                        "Blue"
+                        <input
+                            type="radio"
+                            name="color"
+                            value="blue"
+                            bind:group=favorite_color
+                        />
+                    </label>
+                </fieldset>
+                <div>
+                    <h2>
+                        "Results"
+                    </h2>
                     <p>
-                        "You'll receive cool bonus content!"
+                        "Your favorite color is: " {favorite_color}
                     </p>
-                </Show>
+                    <p>
+                        "Name is: " {name}
+                    </p>
+                    <p>
+                        "Email is: " {email}
+                    </p>
+                    <Show
+                        when=move || spam_me.get()
+                    >
+                        <p>
+                            "You'll receive cool bonus content!"
+                        </p>
+                    </Show>
+                </div>
             </div>
         </div>
     }
