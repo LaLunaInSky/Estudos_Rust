65a34dc65fea75f2e49bd3ca3b791cd22f36b34d
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-26 19:23:14 -0300

    26/06/2025 19:23

--- Estudos/Livro_Rust/Projetos/async_cap_four/src/main.rs
+++ Estudos/Livro_Rust/Projetos/async_cap_four/src/main.rs
@@ -1,7 +1,27 @@
 use std::{
     process::Command,
 };
-use trpl::StreamExt;
+use trpl::{
+    StreamExt,
+    ReceiverStream,
+    Stream
+};
+
+fn get_message() -> impl Stream<Item = String> {
+    let (tx, rx) = trpl::channel();
+
+    let messages = [
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"
+    ];
+
+    for message in messages {
+        tx.send(format!(
+            "Message: '{message}'"
+        )).unwrap();
+    }
+    
+    ReceiverStream::new(rx)
+}
 
 fn clean_terminal_linux() {
     Command::new("clear").status().unwrap();
@@ -9,31 +29,42 @@ fn clean_terminal_linux() {
 
 fn main() {
     clean_terminal_linux();
-    
+
     // Example 1
     trpl::run(async {
         let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         let iter = values.iter().map(|n| n * 2);
         let mut stream = trpl::stream_from_iter(iter);
-        
+
         while let Some(value) = stream.next().await {
             println!("The value was:  {value}");
         }
     });
-    
+
     // Example 2
     println!();
-    
+
     trpl::run(async {
         let values = 1..101;
         let iter = values.map(|n| n * 2);
         let stream = trpl::stream_from_iter(iter);
-        
+
         let mut filtered = stream
                 .filter(|value| value % 3 == 0 || value % 5 == 0);
-        
+
         while let Some(value) = filtered.next().await {
             println!("The value was: {value}");
         }
+    });
+
+    // Example 3
+    println!();
+
+    trpl::run(async {
+       let mut messages = get_message();
+
+       while let Some(message) = messages.next().await {
+           println!("{message}");
+       }
     });
 }
