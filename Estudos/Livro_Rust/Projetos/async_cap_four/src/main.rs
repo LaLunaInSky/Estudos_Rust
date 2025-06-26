7b0889e8934f8fd8fd88107d973b294afe09dbfa
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-26 19:12:00 -0300

    26/06/2025 19:11

--- Estudos/Livro_Rust/Projetos/async_cap_four/src/main.rs
+++ Estudos/Livro_Rust/Projetos/async_cap_four/src/main.rs
@@ -10,6 +10,7 @@ fn clean_terminal_linux() {
 fn main() {
     clean_terminal_linux();
     
+    // Example 1
     trpl::run(async {
         let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         let iter = values.iter().map(|n| n * 2);
@@ -17,6 +18,22 @@ fn main() {
         
         while let Some(value) = stream.next().await {
             println!("The value was:  {value}");
+        }
+    });
+    
+    // Example 2
+    println!();
+    
+    trpl::run(async {
+        let values = 1..101;
+        let iter = values.map(|n| n * 2);
+        let stream = trpl::stream_from_iter(iter);
+        
+        let mut filtered = stream
+                .filter(|value| value % 3 == 0 || value % 5 == 0);
+        
+        while let Some(value) = filtered.next().await {
+            println!("The value was: {value}");
         }
     });
 }
