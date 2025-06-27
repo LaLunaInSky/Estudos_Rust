cec544ca86ddd79a5578239b99a1b81a640ba172
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-27 19:38:23 -0300

    27/06/2025 19:38

--- Projetos/gerenciador_de_projetos_de_estudo/src/main.rs
+++ Projetos/gerenciador_de_projetos_de_estudo/src/main.rs
@@ -7,7 +7,14 @@ fn clean_terminal_linux() {
 fn main() {
     clean_terminal_linux();
 
-    println!("- Gerenciador De Projetos De Estudo Rust -");
-    println!("             Por LaLunaInSky              ");
+    let frase_01: String = String::from("- Gerenciador De Projetos De Estudo Rust -");
+    let limite_de_caracter: usize = frase_01.len();
+
+    let frase_02: String = String::from("Por LaLunaInSky");
+    let tamanho_frase_02: usize = frase_02.len();
+    let quanto_falta_para_o_limite_de_caracter: usize = limite_de_caracter - tamanho_frase_02;
+
+    println!("{}\n{}", frase_01, frase_02);
     println!();
+    println!("{} - {} - {}", limite_de_caracter, tamanho_frase_02, quanto_falta_para_o_limite_de_caracter);
 }
