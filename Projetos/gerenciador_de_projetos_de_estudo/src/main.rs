cf46ef2780cb59380c3051a570b294ce958f24c3
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-27 19:44:23 -0300

    27/06/2025 19:44

--- Projetos/gerenciador_de_projetos_de_estudo/src/main.rs
+++ Projetos/gerenciador_de_projetos_de_estudo/src/main.rs
@@ -13,8 +13,21 @@ fn main() {
     let frase_02: String = String::from("Por LaLunaInSky");
     let tamanho_frase_02: usize = frase_02.len();
     let quanto_falta_para_o_limite_de_caracter: usize = limite_de_caracter - tamanho_frase_02;
+    let metade_do_que_falta_do_limite_de_caracter: usize = quanto_falta_para_o_limite_de_caracter / 2;
+
+    if metade_do_que_falta_do_limite_de_caracter * 2 == quanto_falta_para_o_limite_de_caracter {
+        println!("{} - {}", metade_do_que_falta_do_limite_de_caracter * 2, quanto_falta_para_o_limite_de_caracter);
+    } else {
+        println!("{} - {} - {}", metade_do_que_falta_do_limite_de_caracter * 2, quanto_falta_para_o_limite_de_caracter, quanto_falta_para_o_limite_de_caracter -  (metade_do_que_falta_do_limite_de_caracter * 2))
+    }
 
     println!("{}\n{}", frase_01, frase_02);
     println!();
-    println!("{} - {} - {}", limite_de_caracter, tamanho_frase_02, quanto_falta_para_o_limite_de_caracter);
+    println!(
+        "{} - {} - {} - {}", 
+        limite_de_caracter, 
+        tamanho_frase_02, 
+        quanto_falta_para_o_limite_de_caracter,
+        metade_do_que_falta_do_limite_de_caracter
+    );
 }
