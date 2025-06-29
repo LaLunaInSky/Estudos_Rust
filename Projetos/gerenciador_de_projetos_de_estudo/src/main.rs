94f2ceca56fe8a67dd5dc2e6524589b338237a4e
LaLunaInSky <moonaflordapele@gmail.com>
2025-06-28 21:43:50 -0300

    28/06/2025 21:43

--- Projetos/gerenciador_de_projetos_de_estudo/src/main.rs
+++ Projetos/gerenciador_de_projetos_de_estudo/src/main.rs
@@ -1,18 +1,42 @@
-use std::process::Command;
+use std::{
+    io,
+    process::Command
+};
 
 mod exercicios;
 
-// mod exercícios {
-//     pub mod ex_001 {
-//         fn descrição_do_exercícios() {
-//             println!("Descrição do exercício 001");
-//         }
+fn menu_de_opções_de_exercícios(cabeçalho_do_programa: &String) {
+    loop {
+        let todos_os_exercícios = vec![
+            String::from("ex_001")
+        ];
 
-//         pub fn rodar_o_exercício() {
-//             descrição_do_exercícios();
-//         }
-//     }
-// }
+        println!("{}", cabeçalho_do_programa);
+        
+        println!("          Lista de Exercícios\n");
+        
+        for exercicio in todos_os_exercícios {
+            print!("{exercicio} ");
+        }
+    
+        println!("\nQual exercício você escolhe (coloque apenas o número do exercício)? ");
+    
+        let mut input = String::new();
+    
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                match input.trim().parse::<u32>() {
+                    Ok(number) => println!("Number is {}", number),
+                    Err(_) => {
+                        clean_terminal_linux();
+                    }
+                }
+            }
+            Err(error) => println!("Error: {}", error),
+        }
+    }
+
+}
 
 fn clean_terminal_linux() {
     Command::new("clear").status().unwrap();
@@ -23,7 +47,7 @@ fn main() {
 
     let cabeçalho_do_programa: String = String::from("- Gerenciador De Projetos De Estudo Rust -\n             Por LaLunaInSky               \n");
 
-    println!("{}", cabeçalho_do_programa);
+    menu_de_opções_de_exercícios(&cabeçalho_do_programa);
 
-    exercicios::ex_001::rodar_o_exercício(&cabeçalho_do_programa);
+    // exercicios::ex_001::rodar_o_exercício(&cabeçalho_do_programa);
 }
