9991f0ee251ddf6ad092bad5db23b3b8bc044672
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-17 16:34:47 -0300

    17/07/2025 16:34

--- Estudos/Tauri/Projetos/tauri_app_svelte/src/routes/+page.svelte
+++ Estudos/Tauri/Projetos/tauri_app_svelte/src/routes/+page.svelte
@@ -2,20 +2,28 @@
     import "../app.css";
 
     let name = 'Svelte';
+    let src = "/svelte.svg";
 </script>
 
 <main
     class="
-        bg-gray-700 h-svh flex justify-center items-center
+        bg-gray-700 h-svh flex justify-center items-center *:select-none
     "
 >
     <h1
         class="
-            text-2xl text-neutral-200 font-bold select-none
+            text-2xl text-neutral-200 font-bold
         "
     >
         Hello {name}!
     </h1>
+    <img 
+        {src} 
+        alt="image {name} icon"
+        class="
+            ml-5
+        "
+    >
 </main>
 
 <style lang="postcss">
