ce2ea8b492eecf3a2474a77cd2020a7b586d80f9
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-18 19:02:09 -0300

    18/07/2025 19:02

--- Estudos/Tauri/Projetos/tauri_app_svelte/src/routes/basic_svelte/components/Capture.svelte
+++ Estudos/Tauri/Projetos/tauri_app_svelte/src/routes/basic_svelte/components/Capture.svelte
@@ -1,6 +1,9 @@
 <div
     onkeydowncapture={(e) => alert(`<div> ${e.key}`)}
     role="presentation"
+    class="
+        mb-2 mt-4 bg-slate-700/70 *:text-center *:text-neutral-50 border-2 border-slate-50/40 *:py-1 rounded-lg
+    "
 >
     <input
         onkeydowncapture={(e) => alert(`<input> ${e.key}`)}
