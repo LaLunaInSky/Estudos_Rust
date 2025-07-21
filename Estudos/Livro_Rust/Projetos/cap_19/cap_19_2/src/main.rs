14ddcd35b6fded00baa6141e2f03e8b4583a2519
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-21 19:23:31 -0300

    21/07/2025 19:23

--- Estudos/Livro_Rust/Projetos/cap_19/cap_19_2/src/main.rs
+++ Estudos/Livro_Rust/Projetos/cap_19/cap_19_2/src/main.rs
@@ -1,3 +1,22 @@
+use std::{
+    process::Command
+};
+
+fn clean_terminal_at_linux() {
+    Command::new("clear").status().unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    // clean_terminal_at_linux();
+
+    let some_option_value: Option<i32> = None;
+
+    // let Some(x) = some_option_value;
+    let Some(x) = some_option_value else {
+        return;
+    };
+
+    let x = 5 else {
+        return;
+    };
 }
