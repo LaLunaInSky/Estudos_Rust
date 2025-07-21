108037fe8822f9b423ed37aac926e085fbb56e61
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-21 20:15:14 -0300

    21/07/2025 20:15

--- Estudos/Livro_Rust/Projetos/cap_19/cap_19_3/src/main.rs
+++ Estudos/Livro_Rust/Projetos/cap_19/cap_19_3/src/main.rs
@@ -1,3 +1,138 @@
 fn main() {
-    println!("Hello, world!");
+    let x_01 = 1;
+    
+    match x_01 {
+        1 => println!("One"),
+        2 => println!("Two"),
+        3 => println!("Three"),
+        _ => println!("Anything"),
+    }
+
+    //
+    println!();
+
+    let x_02 = Some(5);
+    let y_01 = 10;
+
+    match x_02 {
+        Some(50) => println!("Got 50"),
+        Some(y_01) => println!("Matched,  y = {}", y_01),
+        _ => println!("Default case, x = {:?}", x_02),
+    }
+
+    println!("at the end: x = {:?}, y = {}", x_02, y_01);
+
+    //
+    println!();
+
+    let x_03 = 1;
+
+    match x_03 {
+        1 | 2 => println!("One or Two!"),
+        3 => println!("Three"),
+        _ => println!("Anything"),
+    }
+
+    //
+    println!();
+
+    let x_04 = 4;
+
+    match x_04 {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    //
+    println!();
+
+    let x_05 = 'c';
+
+    match x_05 {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    // Destructuring Structs
+    println!();
+
+    let p_01 = Point{
+        x: 0, y: 7
+    };
+
+    let Point { x: a, y: b } = p_01;
+
+    let Point { x, y } = p_01;
+    
+    println!(
+        "a == 0 ? {}\nb == 7 ? {}\n",
+        a == 0,
+        b == 7
+    );
+
+    println!(
+        "x == 0 ? {}\ny == 7 ? {}\n",
+        x == 0,
+        y == 7
+    );
+
+    match p_01 {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
+
+    // Destructuring Enums
+    println!();
+
+    let msg_01 = Message::ChangeColor(
+        0, 160, 255
+    );
+
+    match msg_01 {
+        Message::Quit => {
+            println!(
+                "The quit variant has no data to desctructure."
+            );
+        }
+        Message::Move { x, y } => {
+            println!(
+                "Move in the x direction {x} and in the y direction {y}"
+            );
+        }
+        Message::Write(text) => {
+            println!(
+                "The message: {text}"
+            );
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!(
+                "Change color to red {r}, green {g}, and blue {b}"
+            );
+        }
+    }
+}
+
+// Destructuring Structs
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+// Destructuring Enums
+enum Message {
+    Quit,
+    Move {
+        x: i32, 
+        y:i32 
+    },
+    Write(String),
+    ChangeColor(
+        i32, 
+        i32,
+        i32
+    ),
 }
