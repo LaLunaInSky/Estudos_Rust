c842bcb21152dc9dd2bcb439c583ffc957538783
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-24 22:48:40 -0300

    24/07/2025 22:48

--- Estudos/Sqlite/Testes/teste_sqlite_01/src/main.rs
+++ Estudos/Sqlite/Testes/teste_sqlite_01/src/main.rs
@@ -3,10 +3,5 @@ use sqlite::*;
 fn main() {
     let connection = sqlite::open("sharks.db").unwrap();
 
-    let querry_view_table_sharks = "SELECT * FROM sharks";
-
-    connection.iterate(querry_view_table_sharks, |pairs| {
-        println!("{:?}", pairs.unwrap())
-        true
-    }).unwrap();
+    let cursor = connection::cursor();
 }
