a1478e1afc5bb5a75a4318ad74f42e316138d844
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-24 22:38:14 -0300

    24/07/2025 22:38

--- Estudos/Sqlite/Testes/teste_sqlite_01/src/main.rs
+++ Estudos/Sqlite/Testes/teste_sqlite_01/src/main.rs
@@ -1,3 +1,12 @@
+use sqlite::*;
+
 fn main() {
-    println!("Hello, world!");
+    let connection = sqlite::open("sharks.db").unwrap();
+
+    let querry_view_table_sharks = "SELECT * FROM sharks";
+
+    connection.iterate(querry_view_table_sharks, |pairs| {
+        println!("{:?}", pairs.unwrap())
+        true
+    }).unwrap();
 }
