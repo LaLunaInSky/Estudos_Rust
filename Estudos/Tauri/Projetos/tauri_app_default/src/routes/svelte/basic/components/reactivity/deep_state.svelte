521b0c58b9ceba894fe6768588f0dfc0d5531c8e
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-27 18:09:24 -0300

    27/07/2025 18:09

--- Estudos/Tauri/Projetos/tauri_app_default/src/routes/svelte/basic/components/reactivity/deep_state.svelte
+++ Estudos/Tauri/Projetos/tauri_app_default/src/routes/svelte/basic/components/reactivity/deep_state.svelte
@@ -2,8 +2,41 @@
     lang="ts"
 >
     let numbers = $state([1, 2, 3, 4]);
+
+    /* derived state */
+    let total = $derived(
+        numbers.reduce((t, n) => t + n, 0)
+    );
+
+    function addNumber() {
+        numbers.push(numbers.length + 1);
+    }
 </script>
 
-<div>
-    
+<div
+    class="
+        text-neutral-300 flex flex-col gap-2 w-40
+    "
+>
+    <div
+        class="
+            flex justify-center items-center
+        "
+    >
+        <button
+            class="
+                bg-indigo-600/30 px-3 py-1 font-bold cursor-pointer hover:bg-indigo-400/30 rounded-lg border-2 border-indigo-300/20 hover:text-indigo-200/40 
+            "
+            onclick={addNumber}
+        >
+            Add a number
+        </button>
+    </div>
+    <p
+        class="
+            text-center
+        "
+    >
+        {numbers.join(" + ")} = { total }
+    </p>
 </div>
