0e342626355f2c69f323322055c80fb0eacd9fbd
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-27 11:33:25 -0300

    27/07/2025 11:33

---
+++ Estudos/Tauri/Projetos/tauri_app_default/src/routes/svelte/basic/components/introduction/nested_components.svelte
@@ -0,0 +1,9 @@
+<div>
+    <p
+        class="
+            text-neutral-300
+        "
+    >
+        This is another paragraph.
+    </p>
+</div>
--- Estudos/Tauri/Projetos/tauri_app_default/src/routes/svelte/basic/components/introduction/styling.svelte
+++ Estudos/Tauri/Projetos/tauri_app_default/src/routes/svelte/basic/components/introduction/styling.svelte
@@ -1,4 +1,14 @@
-<div>
+<script
+    lang="ts"
+>
+    import NestedComponents from "./nested_components.svelte";
+</script>
+
+<div
+    class="
+        flex flex-col justify-center items-center
+    "
+>
     <p
         class="
             text-amber-400 text-3xl font-[Comic_Sans_MS] font-cus
@@ -6,4 +16,5 @@
     >
         This is a paragraph.
     </p>
+    <NestedComponents />
 </div>
