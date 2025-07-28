946a2cc625b2e267e85de8e75ae99a2e20fe8eaa
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-28 18:20:04 -0300

    28/07/2025 18:20

--- Estudos/Livro_Rust/Projetos/cap_20/cap_20_1/src/main.rs
+++ Estudos/Livro_Rust/Projetos/cap_20/cap_20_1/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    // 20-1
+    let mut num = 5;
+
+    let r1 = &raw const num;
+    let r2 = &raw mut num;
+
+    // 20-2
+    let address = 0x012345usize;
+    let r = address as *const i32;
+
+    //20-3
+    unsafe {
+        println!(
+            "r1 is: {}\nr2 is: {}",
+            *r1, *r2
+        );
+    }
 }
