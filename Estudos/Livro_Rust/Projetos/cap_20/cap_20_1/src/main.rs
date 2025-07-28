be521afdb832f4d501edac819816e867989338cd
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-28 19:46:12 -0300

    28/07/2025 19:46

--- Estudos/Livro_Rust/Projetos/cap_20/cap_20_1/src/main.rs
+++ Estudos/Livro_Rust/Projetos/cap_20/cap_20_1/src/main.rs
@@ -1,3 +1,5 @@
+use std::slice;
+
 fn main() {
     // 20-1
     let mut num = 5;
@@ -15,5 +17,52 @@ fn main() {
             "r1 is: {}\nr2 is: {}",
             *r1, *r2
         );
+    }
+
+    unsafe fn dangerous() {}
+
+    unsafe {
+        dangerous();
+    }
+
+    // 20-4
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+
+    println!(
+        "\n{:?}\n{:?}",
+        a, b
+    );
+
+    // 20-5
+    fn split_at_mut(
+        values: &mut [i32], mid: usize
+    ) -> (
+        &mut [i32],
+        &mut [i32]
+    ) {
+        let len = values.len();
+        
+        assert!(mid <= len);
+
+        // 20-6
+        let ptr = values.as_mut_ptr();
+
+        unsafe {
+            (
+                slice::from_raw_parts_mut(
+                    ptr, mid
+                ),
+                slice::from_raw_parts_mut(
+                    ptr.add(mid), len - mid
+                ),
+            )
+        }
     }
 }
