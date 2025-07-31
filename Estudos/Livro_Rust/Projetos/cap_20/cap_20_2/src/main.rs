eef5776b39b3dbdb108262c28b8b86ba8e1cddbb
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-30 21:25:17 -0300

    30/07/2025 21:25

--- Estudos/Livro_Rust/Projetos/cap_20/cap_20_2/src/main.rs
+++ Estudos/Livro_Rust/Projetos/cap_20/cap_20_2/src/main.rs
@@ -22,17 +22,91 @@ impl Add for Point {
 struct Millimeters(u32);
 struct Meters(u32);
 
-impl Add<Meters> for Millimeters [
+impl Add<Meters> for Millimeters {
     type Output = Millimeters;
 
     fn add(self, other: Meters) -> Millimeters {
         Millimeters(self.0 + (other.0 * 1000))
     }
-]
+}
+
+// 20-17
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!(
+            "This is your captain speaking."
+        );
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!(
+            "Up!"
+        );
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!(
+            "*waving arms furiously*"
+        );
+    }
+}
+
+// 20-20
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
 
 fn main() {
     assert_eq!(
         Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
         Point { x: 3, y: 3 }
+    );
+
+    // 20-18
+    let person = Human;
+    person.fly();
+
+    // 20-19
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+
+    // 20-20
+    println!(
+        "\nA baby dog is called a {}",
+        Dog::baby_name()
+    );
+
+    // 20-22
+    println!(
+        "\nA baby dog is called a {}",
+        <Dog as Animal>::baby_name()
     );
 }
