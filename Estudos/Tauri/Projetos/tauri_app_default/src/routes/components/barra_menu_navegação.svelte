fc04ceb9df399c49ab9b1307e76846b2a9fe2900
LaLunaInSky <moonaflordapele@gmail.com>
2025-08-05 14:07:52 -0300

    05/08/2025 14:07

--- Estudos/Tauri/Projetos/tauri_app_default/src/routes/components/barra_menu_navegação.svelte
+++ Estudos/Tauri/Projetos/tauri_app_default/src/routes/components/barra_menu_navegação.svelte
@@ -32,7 +32,7 @@
 >
     <div
         class="
-            bg-slate-600/60 w-10/11 rounded-b-xl h-14 flex gap-6 justify-center items-center uppercase text-neutral-200 text-md font-bold *:hover:bg-neutral-50/70 *:hover:text-neutral-800 fixed *:p-2 *:transition-colors *:hover:transition-colors *:rounded-xl *:hover:underline-2 *:hover:underline-offset-4 select-none
+            bg-slate-600/60 w-10/11 rounded-b-xl h-14 flex gap-6 justify-center items-center uppercase text-neutral-200 text-md font-bold *:hover:bg-neutral-50/70 *:hover:text-neutral-800 fixed *:p-2 *:transition-colors *:hover:transition-colors *:rounded-lg *:hover:underline-2 *:hover:underline-offset-4 select-none
         "
     >
         {#each rotas_menus as rota}
