1d74b6982867ec5ec84f7ccf4213fc47dbf94c15
LaLunaInSky <moonaflordapele@gmail.com>
2025-08-25 22:15:05 -0300

    25/08/2025 22:15

---
+++ Estudos/Leptos/Aulas/Aulas_02/Aula_02_01/src/main.rs
@@ -0,0 +1,11 @@
+use leptos::prelude::*;
+
+fn main() {
+    console_error_panic_hook::set_once();
+
+    leptos::mount::mount_to_body(
+        || view! {
+            <p>"Hello, World!"</p>
+        }
+    )
+}
