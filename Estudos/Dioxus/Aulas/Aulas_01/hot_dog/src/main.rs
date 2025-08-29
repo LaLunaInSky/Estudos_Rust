2dd8d82205546c7d8b77f03b77621e02c42a31bb
LaLunaInSky <moonaflordapele@gmail.com>
2025-08-28 22:46:42 -0300

    28/08/2025 22:46

---
+++ Estudos/Dioxus/Aulas/Aulas_01/hot_dog/src/main.rs
@@ -0,0 +1,42 @@
+use dioxus::prelude::*;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        
+        div {
+            id: "title",
+            h1 {
+                "HotDog! 🌭"
+            }
+        }
+        div {
+            id: "dogview",
+            img {
+                src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg"
+            }
+        }
+        div {
+            id: "buttons",
+            button {
+                id: "skip",
+                "skip"
+            },
+            button {
+                id: "save",
+                "save!"
+            }
+        }
+    }
+}
