f6e25d73dc8c11337114b924c5edb658e9a1ce0f
LaLunaInSky <moonaflordapele@gmail.com>
2025-08-29 00:36:44 -0300

    29/08/2025 00:36

--- Estudos/Dioxus/Aulas/Aulas_01/hot_dog/src/main.rs
+++ Estudos/Dioxus/Aulas/Aulas_01/hot_dog/src/main.rs
@@ -19,28 +19,34 @@ fn App() -> Element {
             rel: "stylesheet", 
             href: TAILWIND_CSS 
         }
-        
-        div {
-            class: "text-red-500",
-            h1 {
-                "HotDog! 🌭"
-            }
-        }
 
-        div {
-            img {
-                src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg"
-            }
-        }
-
-        div {
-            button {
-                id: "skip",
-                "skip"
+        body {
+            class: "bg-black min-h-svh *:select-none",
+            div {
+                class: "text-neutral-100 font-bold text-3xl py-2 text-center border-b-2 mb-2",
+                h1 {
+                    "HotDog! 🌭"
+                }
             },
-            button {
-                id: "save",
-                "save!"
+            div {
+                class: "flex justify-center items-center py-2",
+                img {
+                    class: "w-90 shadow-lg/70 shadow-neutral-100 rounded-lg ring-4 ring-neutral-100/70",
+                    src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg"
+                }
+            },
+            div {
+                class: "*:text-neutral-100 flex justify-center items-center gap-4 my-3 *:py-2 *:font-bold *:text-2xl *:px-3 *:rounded-2xl *:hover:bg-neutral-100/80 *:cursor-pointer",
+                button {
+                    class: "bg-red-800 hover:text-red-800",
+                    id: "skip",
+                    "skip"
+                },
+                button {
+                    class: "bg-green-800 hover:text-green-800",
+                    id: "save",
+                    "save!"
+                }
             }
         }
     }
